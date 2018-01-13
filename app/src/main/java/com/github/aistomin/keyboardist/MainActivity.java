f11dff0097fde3ce6f7807c1aefee8dd7ef17096
aistomin <andrej.istomin.ikeen@gmail.com>
2018-01-13 15:09:45 +0100

    displayed text format was slightly improved.

--- app/src/main/java/com/github/aistomin/keyboardist/MainActivity.java
+++ app/src/main/java/com/github/aistomin/keyboardist/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.widget.TextView;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +17,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean dispatchKeyEvent(final KeyEvent event) {
         if (event.getAction() == KeyEvent.ACTION_DOWN) {
-            ((TextView) findViewById(R.id.tvDisplay)).setText(event.toString());
+            ((TextView) findViewById(R.id.tvDisplay)).setText(
+                String.format(
+                    Locale.getDefault(), "%s: %d\n%s: %s",
+                    getString(R.string.key_code), event.getKeyCode(),
+                    getString(R.string.key_details), event.toString()
+                )
+            );
         }
         return super.dispatchKeyEvent(event);
     }
